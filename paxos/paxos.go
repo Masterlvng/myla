315d8e1eae9dda24f2e6edc6d89994277a95a2e9
masterlvng <masterlvng@gmail.com>
2017-02-26 21:39:11 +0800

    add comment

--- paxos/paxos.go
+++ paxos/paxos.go
@@ -1,10 +1,17 @@
 package paxos
 
+import (
+	"github.com/masterlvng/myla/node"
+	"github.com/masterlvng/myla/proto/paxosmsg"
+)
+
+//Ballot ...
 type Ballot struct {
 	ProposalID uint64
 	NodeID     uint64
 }
 
+//Eq ...
 func (b *Ballot) Eq(other Ballot) bool {
 	if b.ProposalID == other.ProposalID && b.NodeID == other.ProposalID {
 		return true
@@ -12,6 +19,7 @@ func (b *Ballot) Eq(other Ballot) bool {
 	return false
 }
 
+//NoLT ...
 func (b *Ballot) NoLT(other Ballot) bool {
 	if b.ProposalID == other.ProposalID {
 		return b.NodeID >= other.NodeID
@@ -19,10 +27,12 @@ func (b *Ballot) NoLT(other Ballot) bool {
 	return b.ProposalID >= other.ProposalID
 }
 
+//IsNULL ..
 func (b *Ballot) IsNULL() bool {
 	return b.ProposalID == 0
 }
 
+//Reset ...
 func (b *Ballot) Reset() {
 	b.ProposalID = 0
 	b.NodeID = 0
@@ -36,10 +46,10 @@ func (b *base) NewInstance() {
 
 }
 
-func (b *base) SendMsg() {
+func (b *base) SendMsg(nid node.ID) {
 
 }
 
-func (b *base) BroadcastMsg() {
+func (b *base) BroadcastMsg(msg *paxosmsg.PaxosMsg) {
 
 }
