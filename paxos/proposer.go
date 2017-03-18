9b37f4089583aea694c6c08a5e1b81906215ff80
masterlvng <masterlvng@gmail.com>
2017-03-18 09:50:52 +0800

    add function prototype

--- paxos/proposer.go
+++ paxos/proposer.go
@@ -1,1 +1,43 @@
 package paxos
+
+type Proposer struct {
+	ProposerID                  uint64
+	HighestOtherProposerID      uint64
+	Proposal                    []byte
+	HighestOtherPreAcceptBallot Ballot
+}
+
+func (p *Proposer) AddPreAcceptValue(otherPreAcceptBallot *Ballot, otherPreAcceptProposal []byte) {
+
+}
+
+func (p *Proposer) ResetHighestOtherPreAcceptBallot() {
+	p.HighestOtherPreAcceptBallot.Reset()
+}
+
+func (p *Proposer) Prepare() {
+
+}
+
+func (p *Proposer) OnPrepareReply() {
+}
+
+func (p *Proposer) Accept() {
+
+}
+
+func (p *Proposer) OnAcceptReply() {
+
+}
+
+func (p *Proposer) ExitPrepare() {
+
+}
+
+func (p *Proposer) ExitAccept() {
+
+}
+
+func (p *Proposer) CancelSkipPrepare() {
+
+}
